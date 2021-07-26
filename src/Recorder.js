c14858ab42c38f6d733ddb1b48b95d0a16e80558
Pot_Lid <p8032336@gmail.com>
2021-07-26 11:52:30 -0400

    work on recorder.js

---
+++ src/Recorder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Recorder extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Recorder;
