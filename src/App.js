1641353f0c89595e7d9b4d482705b4d0139735c3
PotLid <p8032336@gmail.com>
2021-08-02 14:19:29 -0400

    minor changes to the demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
 import Recorder from "./Recorder";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <p>Recorder</p>
+          <p>Recorder Demo</p>
       </header>
       <Recorder />
     </div>
